ebd970bda7e2dabb3f0e36b0ef970a3953b37e37
PollyT249 <polinatretiakova249@gmail.com>
2024-06-24 21:10:39 +0300

    Removed cosole.log

--- src/index.js
+++ src/index.js
@@ -69,8 +69,6 @@ function getForecast(city) {
 }
 
 function displayForecast(response) {
-  console.log(response.data);
-
   let forecastHtml = "";
 
   response.data.daily.forEach(function (day, index) {
